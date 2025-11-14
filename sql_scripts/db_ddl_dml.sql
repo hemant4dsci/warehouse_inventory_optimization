78a10632751f6216467a4d46b14166717309dce5
hemant4dsci <hemant4dsci@gmail.com>
2025-11-14 19:05:21 +0530

    script to create db

--- sql_scripts/db_ddl_dml.sql
+++ sql_scripts/db_ddl_dml.sql
@@ -1,112 +1,74 @@
 -- Create data base
-CREATE DATABASE warehouse_db;
+CREATE DATABASE warehouse_inventory;
 
--- Define structure for employee table
-CREATE TABLE IF NOT EXISTS employees (
-    picker_id CHAR(7) PRIMARY KEY,
-    employee_name VARCHAR(20),
-    shift VARCHAR(7)
+-- Define structure for suppliers table
+CREATE TABLE IF NOT EXISTS suppliers (
+    supplier_id SMALLINT PRIMARY KEY,
+    supplier_name VARCHAR(15),
+    supplier_location VARCHAR(10),
+    avg_lead_time_days SMALLINT,
+    on_time_delivery_rate NUMERIC(5, 2)
 );
 
--- Inserting data into employee table
-COPY employees
+-- Inserting data into suppliers table
+COPY suppliers
 FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\employees.csv'
+    'E:\Data Analytics Projects\warehouse_inventory_optimization_analytics\data\raw\suppliers.csv'
 WITH
     (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
--- Define sturcture for employee performance table
-CREATE TABLE IF NOT EXISTS employee_performance (
-    picker_id CHAR(7),
-    record_date DATE,
-    orders_picked SMALLINT,
-    errors SMALLINT,
-    hours_worked NUMERIC(3, 1),
-    FOREIGN KEY (picker_id) REFERENCES employees (picker_id)
-);
-
--- Insert data into employee Performance table
-COPY employee_performance
-FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\employee_performance.csv'
-WITH
-    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
-
--- Define structure for products table
-CREATE TABLE IF NOT EXISTS products (
-    sku_id CHAR(8) PRIMARY KEY,
-    category VARCHAR(15),
-    weight_kg NUMERIC(5, 2),
-    dimension_cm VARCHAR(9),
-    supplier VARCHAR(35)
-);
-
--- Insert data into products table
-COPY products
-FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\products.csv'
-WITH
-    (FORMAT CSV, HEADER TRUE, DELIMITER ',');
-
--- Define structure for inventory table
+-- Define sturcture for inventory table
 CREATE TABLE IF NOT EXISTS inventory (
-    sku_id CHAR(8),
-    record_date DATE,
-    opening_stock INTEGER,
-    closing_stock INTEGER,
+    item_id CHAR(8) PRIMARY KEY,
+    item_name VARCHAR(12),
+    category VARCHAR(12),
+    warehouse_zone CHAR(1),
+    bin_location CHAR(5),
+    current_stock SMALLINT,
     reorder_level SMALLINT,
-    replenishment_lead_time SMALLINT,
-    unit_cost NUMERIC(7, 2),
-    FOREIGN KEY (sku_id) REFERENCES products (sku_id)
+    unit_cost NUMERIC(10, 2),
+    batch_number CHAR(10),
+    date_expiry DATE
 );
 
--- Insert data into inventory table
+-- Insert data into employee inventory table
 COPY inventory
 FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\inventory.csv'
+    'E:\Data Analytics Projects\warehouse_inventory_optimization_analytics\data\raw\inventory.csv'
 WITH
     (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
--- Define structure for orders table
-CREATE TABLE IF NOT EXISTS orders (
-    order_id CHAR(9) PRIMARY KEY,
+-- Define structure for purchase_orders table
+CREATE TABLE IF NOT EXISTS purchase_orders (
+    po_id CHAR(8) PRIMARY KEY,
+    item_id CHAR(8),
+    order_qty SMALLINT,
     order_date DATE,
-    ship_date DATE,
-    sku_id CHAR(8),
-    quantity SMALLINT,
-    order_zone CHAR(1),
-    picker_id CHAR(7),
-    process_time_min SMALLINT,
-    order_value NUMERIC(10, 2),
-    on_time_flag CHAR(1),
-    order_priority VARCHAR(6),
-    FOREIGN KEY (sku_id) REFERENCES products (sku_id),
-    FOREIGN KEY (picker_id) REFERENCES employees (picker_id)
+    recieved_date DATE,
+    supplier_id SMALLINT,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers (supplier_id),
+    FOREIGN KEY (item_id) REFERENCES inventory (item_id)
 );
 
--- Insert data into orders table
-COPY orders
+-- Insert data into purchase_orders table
+COPY purchase_orders
 FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\orders.csv'
+    'E:\Data Analytics Projects\warehouse_inventory_optimization_analytics\data\raw\purchase_orders.csv'
 WITH
     (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
--- Define structure for shipments table
-CREATE TABLE IF NOT EXISTS shipments (
-    shipment_id CHAR(13) PRIMARY KEY,
-    order_id CHAR(9),
-    carrier VARCHAR(10),
-    distance_km NUMERIC(7, 2),
-    shipping_cost NUMERIC(7, 2),
-    delivery_time_days SMALLINT,
-    promised_days SMALLINT,
-    delivery_status VARCHAR(10),
-    FOREIGN KEY (order_id) REFERENCES orders (order_id)
+-- Define structure for sales table
+CREATE TABLE IF NOT EXISTS sales (
+    sale_id CHAR(7) PRIMARY KEY,
+    item_id CHAR(8),
+    sold_qty SMALLINT,
+    sale_date DATE,
+    FOREIGN KEY (item_id) REFERENCES inventory (item_id)
 );
 
--- Insert data into shipments table
-COPY shipments
+-- Insert data into sales table
+COPY sales
 FROM
-    'E:\Data Analytics Projects\warehouse_optimization_analytics\data\raw\shipments.csv'
+    'E:\Data Analytics Projects\warehouse_inventory_optimization_analytics\data\raw\sales.csv'
 WITH
     (FORMAT CSV, HEADER TRUE, DELIMITER ',');
